d391a3fc61657b60b6e016eb6b9c1867ccae52ef
wwknt0782 <204444020+wwknt0782@users.noreply.github.com>
2025-06-11 21:01:15 +0900

    環境構築

---
+++ my-app/src/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+    return (
+        <div>
+            <div>
+                {/*ヘッダー*/}
+                <h1>ヘッダー</h1>
+            </div>
+
+            <div>
+                {/*メイン*/}
+                <div>{/*入力画面*/}</div>
+                <div>{/*タスクリスト 見出し*/}</div>
+                <div>{/*タスクリスト*/}</div>
+            </div>
+
+            <div>{/*フッター*/}</div>
+        </div>
+    );
+}
---
+++ my-app/src/features/task/components/ui/AddTaskButton.tsx
@@ -0,0 +1,3 @@
+export const AddTaskButton = () => {
+    return <div></div>;
+};
